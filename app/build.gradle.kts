e9a71c4b8f226f7b0624bd6a6a349dcaad617ba0
edixontech <tecnoedix@icloud.com>
2025-02-21 12:31:18 +0100

    First concept of the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.calculator_imb"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.calculator_imb"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/calculator_imb/MainActivity.kt
+++ app/src/main/java/com/example/calculator_imb/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.calculator_imb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import java.awt.font.TextAttribute
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var heightEditText : EditText
+    lateinit var weightEditText:EditText
+    lateinit var calculateBottomBar: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        heightEditText= findViewById(R.id.heigtEditText)
+        weightEditText= findViewById(R.id.weigtEditTex)
+        calculateBottomBar = findViewById(R.id.button)
+
     }
 }
